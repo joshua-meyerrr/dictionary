a9463836534b5e309f21d904f12032ed3e51d252
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-08-06 10:58:06 +0200

    Import createElement and create main

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import './style.css';
+import { createElement } from './utils/createElement';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
+const mainEl = createElement('main', {
+  childElements: [
+    createElement('h1', {
+      innerText: '<cool name here>',
+      className: 'heading',
+    }),
+  ],
+});
 
 if (app !== null) {
-  app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+  app.append(mainEl);
 }
