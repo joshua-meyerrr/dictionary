8835e6dfe03b6ebb3240aa99ff7a40f8526e7e8c
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-08-06 14:38:42 +0200

    💄 Add Entry-Styling

--- src/components/entry/entry.ts
+++ src/components/entry/entry.ts
@@ -5,40 +5,51 @@ export function createEntry() {
   return createElement('article', {
     className: styles.wrapper,
     childElements: [
-      createElement('h2', {
-        className: styles.entry__heading,
-        innerText: 'Code',
+      createElement('div', {
+        className: styles.entry__header,
+        childElements: [
+          createElement('h2', {
+            className: styles.header__heading,
+            innerText: 'Code',
+          }),
+          createElement('p', {
+            innerText: '/koʊd/',
+          }),
+        ],
       }),
-      createElement('p', {
-        innerText: '/koʊd/',
-      }),
-
       createElement('div', {
         className: styles.entry__list,
         childElements: [
           createElement('section', {
-            className: styles.list__entry,
+            className: styles.list__section,
             childElements: [
               createElement('p', {
-                innerText: 'Type: Verb',
+                className: styles.section__type,
+                innerText: 'Verb',
               }),
               createElement('div', {
+                className: styles.section__definition,
                 childElements: [
                   createElement('p', {
-                    innerText: 'definiton:',
+                    className: styles.definition__heading,
+                    innerText: 'Definiton:',
                   }),
                   createElement('p', {
+                    className: styles.definition__text,
                     innerText:
                       'Convert (the words of a message) into a particular code in order to convey a secret meaning.',
                   }),
                 ],
               }),
               createElement('div', {
+                className: styles.section__example,
                 childElements: [
                   createElement('p', {
-                    innerText: 'example',
+                    className: styles.example__heading,
+                    innerText: 'Example',
                   }),
                   createElement('p', {
+                    className: styles.example__text,
                     innerText:
                       'only Mitch knew how to read the message—even the name was coded',
                   }),
