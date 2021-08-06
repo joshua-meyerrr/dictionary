7a4c8b26aa4f7320fae598e04aea3565031f0695
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-08-06 11:29:23 +0200

    Add example entry

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,44 @@ import { createElement } from './utils/createElement';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
+const definition = {
+  word: 'code',
+  phonetics: '/koʊd/',
+  sound: 'play',
+  meanings: [
+    {
+      type: 'verb',
+      definitions: [
+        {
+          definition:
+            'Convert (the words of a message) into a particular code in order to convey a secret meaning.',
+          example:
+            'only Mitch knew how to read the message—even the name was coded',
+        },
+        {
+          definition: 'Write code for (a computer program)',
+          example: 'most developers code C + + like C',
+        },
+      ],
+    },
+    {
+      type: 'noun',
+      definitions: [
+        {
+          definition:
+            'A system of words, letters, figures, or other symbols substituted for other words, letters, etc., especially for the purposes of secrecy.',
+          example:
+            'only Mitch knew how to read the message—even the name was coded',
+        },
+        {
+          definition: 'Write code for (a computer program)',
+          example: 'most developers code C + + like C',
+        },
+      ],
+    },
+  ],
+};
+
 const mainEl = createElement('main', {
   childElements: [
     createElement('h1', {
