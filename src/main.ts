2fe1bb3e4ac259b87211cd48603d1ce83ed75904
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-08-09 17:09:17 +0200

    🐛 Add workaround for top-level await

--- src/main.ts
+++ src/main.ts
@@ -4,67 +4,70 @@ import { Entry } from './types';
 import { createElement } from './utils/createElement';
 import { getEntry } from './utils/api';
 
-const app = document.querySelector<HTMLDivElement>('#app');
+async function run() {
+  const app = document.querySelector<HTMLDivElement>('#app');
 
-const defaultEntry: Entry = {
-  word: 'doublethink',
-  phonetics: 'ˈdə-bəl-ˌthiŋk',
-  meanings: [
-    {
-      type: 'noun',
-      text: {
-        definition:
-          'The acceptance of contrary opinions or beliefs at the same time, especially as a result of political indoctrination.',
-        example:
-          'By an amazing exploit of doublethink, he manages to convince himself that theistic explanations are simple explanations.',
+  const defaultEntry: Entry = {
+    word: 'doublethink',
+    phonetics: 'ˈdə-bəl-ˌthiŋk',
+    meanings: [
+      {
+        type: 'noun',
+        text: {
+          definition:
+            'The acceptance of contrary opinions or beliefs at the same time, especially as a result of political indoctrination.',
+          example:
+            'By an amazing exploit of doublethink, he manages to convince himself that theistic explanations are simple explanations.',
+        },
       },
+    ],
+  };
+
+  const entryList = createElement('div', {
+    className: styles.results,
+    childElements: [createEntry(defaultEntry)],
+  });
+
+  const inputBar = createElement('input', {
+    placeholder: 'look up a word...',
+    className: styles.search__bar,
+  });
+
+  inputBar.addEventListener('keyup', function (event) {
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      inputButton.click();
+    }
+  });
+
+  const inputButton = createElement('button', {
+    className: styles.search__button,
+    innerText: 'search',
+    onclick: async () => {
+      entryList.innerHTML = '';
+      const newWord = await getEntry(inputBar.value);
+      entryList.append(createEntry(newWord[0]));
     },
-  ],
-};
+  });
 
-const entryList = createElement('div', {
-  className: styles.results,
-  childElements: [createEntry(defaultEntry)],
-});
+  const mainEl = createElement('main', {
+    childElements: [
+      createElement('h1', {
+        innerText: 'Vocabulous',
+        className: styles.heading,
+      }),
+      createElement('div', {
+        className: styles.search,
+        childElements: [inputBar, inputButton],
+      }),
+      entryList,
+    ],
+  });
 
-const inputBar = createElement('input', {
-  placeholder: 'look up a word...',
-  className: styles.search__bar,
-});
-
-inputBar.addEventListener('keyup', function (event) {
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    inputButton.click();
+  if (app !== null) {
+    app.append(mainEl);
   }
-});
-
-const inputButton = createElement('button', {
-  className: styles.search__button,
-  innerText: 'search',
-  onclick: async () => {
-    entryList.innerHTML = '';
-    const newWord = await getEntry(inputBar.value);
-    entryList.append(createEntry(newWord[0]));
-  },
-});
-
-const mainEl = createElement('main', {
-  childElements: [
-    createElement('h1', {
-      innerText: 'Vocabulous',
-      className: styles.heading,
-    }),
-    createElement('div', {
-      className: styles.search,
-      childElements: [inputBar, inputButton],
-    }),
-    entryList,
-  ],
-});
-
-if (app !== null) {
-  app.append(mainEl);
 }
+run();
